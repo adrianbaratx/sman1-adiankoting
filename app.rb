4bbb804f561aded7d30d69ab35c30cc551f21931
adrianbaratx <adrianbaratx@gmail.com>
2026-05-08 02:37:49 +0700

    update website per halaman

--- app.rb
+++ app.rb
@@ -1,24 +1,45 @@
-require 'sinatra'
+require "sinatra"
 
-set :bind, '0.0.0.0'
-set :port, 4567
+set :bind, "0.0.0.0"
+set :port, ENV.fetch("PORT", 4567)
 
-get '/' do
+helpers do
+  def active_nav(path)
+    request.path_info == path ? "active" : ""
+  end
+end
+
+get "/" do
+  @title = "Beranda - SMA Negeri 1 Adiankoting"
   erb :index
 end
 
-get '/profil' do
+get "/profil" do
+  @title = "Profil - SMA Negeri 1 Adiankoting"
   erb :profil
 end
 
-get '/akademik' do
+get "/kepala-sekolah" do
+  @title = "Kepala Sekolah - SMA Negeri 1 Adiankoting"
+  erb :kepala_sekolah
+end
+
+get "/akademik" do
+  @title = "Akademik - SMA Negeri 1 Adiankoting"
   erb :akademik
 end
 
-get '/berita' do
+get "/berita" do
+  @title = "Berita - SMA Negeri 1 Adiankoting"
   erb :berita
 end
 
-get '/kontak' do
+get "/galeri" do
+  @title = "Galeri - SMA Negeri 1 Adiankoting"
+  erb :galeri
+end
+
+get "/kontak" do
+  @title = "Kontak - SMA Negeri 1 Adiankoting"
   erb :kontak
 end
